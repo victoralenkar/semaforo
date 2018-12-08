6b4a9997d5d234a074079698fb2356a664a3fe2f
Victor Alencar <vba.cin@gmail.com>
2018-12-08 12:59:54 -0200

    Versão final comentada!

--- src/main/java/br/uff/ic/siscomp/semaforos/Tarefa.java
+++ src/main/java/br/uff/ic/siscomp/semaforos/Tarefa.java
@@ -16,29 +16,22 @@ public class Tarefa {
 			try {
 				// Adquire semáforo para controle das N iterações
 				semaforoControle.acquire();
+				System.out.println("[RECURSO ADQUIRIDO PELA ITERAÇÃO " + (i + 1) + "...]");
 				// Inicia thread para imprimir Vermelho
 				ImpressorVermelho iVermelho = new ImpressorVermelho();
 				iVermelho.start();
-				
+
 				// Inicia thread para imprimir Azul
 				ImpressorAzul iAzul = new ImpressorAzul();
 				iAzul.start();
-				
+
 				// Inicia thread para imprimir Verde
-				ImpressorVerde iVerde = new ImpressorVerde();
+				ImpressorVerde iVerde = new ImpressorVerde(i);
 				iVerde.start();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
-			} finally {
-				// Libera semáforo de controle para próxima iteração
-				semaforoControle.release();
 			}
 		}
-	}
-
-
-	public static void main(String[] args) {
-		new Tarefa(Integer.parseInt(args[0]));
 	}
 
 	public class ImpressorVermelho extends Thread {
@@ -46,15 +39,19 @@ public class Tarefa {
 			try {
 				// Adquire semáforo para impressão de cor
 				semaforoImpressao.acquire();
+				System.out.println("[RECURSO ADQUIRIDO PELA THREAD VERMELHA]");
 				// Sorteia um número aleatório entre 0 e 9
 				int x = new Random().nextInt(10);
 				// Espera x segundos
+				System.out.println("[THREAD VERMELHA DORMINDO POR " + x + " SEGUNDOS...]");
 				sleep(x * 1000);
+
 				System.out.println("Vermelho");
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} finally {
 				// Libera semáforo de impressão de cor
+				System.out.println("[RECURSO LIBERADO PELA THREAD VERMELHA]");
 				semaforoImpressao.release();
 			}
 		}
@@ -65,37 +62,60 @@ public class Tarefa {
 			try {
 				// Adquire semáforo para impressão de cor
 				semaforoImpressao.acquire();
+				System.out.println("[RECURSO ADQUIRIDO PELA THREAD AZUL]");
 				// Sorteia um número aleatório entre 0 e 9
 				int x = new Random().nextInt(10);
 				// Espera x segundos
+				System.out.println("[THREAD AZUL DORMINDO POR " + x + " SEGUNDOS...]");
 				sleep(x * 1000);
 				System.out.println("Azul");
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} finally {
+				System.out.println("[RECURSO LIBERADO PELA THREAD AZUL]");
 				semaforoImpressao.release();
 			}
 		}
 	}
 
 	public class ImpressorVerde extends Thread {
+		private int iteracao;
+
+		public ImpressorVerde(int i) {
+			this.iteracao = i;
+		}
+
 		public void run() {
 			try {
 				// Adquire semáforo para impressão de cor
 				semaforoImpressao.acquire();
+				System.out.println("[RECURSO ADQUIRIDO PELA THREAD VERDE]");
 				// Sorteia um número aleatório entre 0 e 9
 				int x = new Random().nextInt(10);
 				// Espera x segundos
+				System.out.println("[THREAD VERDE DORMINDO POR " + x + " SEGUNDOS...]");
 				sleep(x * 1000);
 				System.out.println("Verde");
+				System.out.println();
 				System.out.println();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} finally {
 				// Libera semáforo de impressão de cor
+				System.out.println("[RECURSO LIBERADO PELA THREAD VERDE]");
 				semaforoImpressao.release();
+				//Libera semáforo de controle para próxima iteração
+				System.out.println("[RECURSO LIBERADO PELA ITERAÇÃO " + (iteracao + 1) + "...]");
+				semaforoControle.release();
+				if (iteracao + 1 == total) {
+					System.out.println("[" + total + " ITERAÇÕES REALIZADAS COM SUCESSO!]");
+				}
 			}
 		}
+	}
+
+	public static void main(String[] args) {
+		new Tarefa(Integer.parseInt(args[0]));
 	}
 
 }
